47e6fb9c7a22eb55b730c61870241eb7cb8fa990
Akshata <akshatakagale1@gmail.com>
2023-11-05 10:36:50 +0530

    deploy

---
+++ Database.sql
@@ -0,0 +1,10 @@
+create database testingdb;
+use testingdb;
+create table info_tab(ClientId int primary key, ClientName varchar(250), Age varchar(250),
+Address varchar(250), Product varchar(250), Price varchar(250), Total varchar(250));
+
+select*from info_tab;
+
+
+
+
